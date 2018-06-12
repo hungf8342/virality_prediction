c3ca6a84e067e30b6c2c751f006b046c0f248637
bosonbaas <bosonbaas@gmail.com>
2018-06-12 14:13:41 -0500

    Cleaned up hawkes

--- models/hawkes.py
+++ models/hawkes.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 
 def propagate(i,theta,E,T):
--- models/test.py
+++
@@ -1,8 +0,0 @@
-import hawkes_sample as hs
-import numpy as np
-
-E = np.loadtxt("karate.txt", dtype='int')
-TE = hs.exact_hawkes(E, 100, 0.1)
-TS = hs.sample_hawkes(E, 1000, 0.1)
-print(TE)
-print(TS)

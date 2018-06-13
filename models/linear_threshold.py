39fb818e5baefde93e4210e1ca2467abab40ac8a
Frances Hung <wyfhung@gmail.com>
2018-06-13 11:25:35 -0400

    fixed updating neighbors
    was previously updating based on current aggregate influence, not whether the node was active or not

--- models/linear_threshold.py
+++ models/linear_threshold.py
@@ -11,11 +11,11 @@ def neighbor_update(edge_matrix,node_matrix,n,active,new_active):
     neighbors=edge_matrix[edge_matrix[:,0]==n,1:]
     now_active=[]
     #weights=neighbors[:,1]
-    n_inf=node_matrix[node_matrix[:,0]==n,2]
+    #n_inf=node_matrix[node_matrix[:,0]==n,2]
     for i in range(0,np.ma.size(neighbors,axis=0)):
         neigh_weight=neighbors[i,1]
         neighbor=neighbors[i,0]
-        node_matrix[node_matrix[:,0]==neighbor,2]=node_matrix[node_matrix[:,0]==neighbor,2]+neigh_weight*n_inf
+        node_matrix[node_matrix[:,0]==neighbor,2]=node_matrix[node_matrix[:,0]==neighbor,2]+neigh_weight
         if node_matrix[node_matrix[:,0]==neighbor,1]<node_matrix[node_matrix[:,0]==neighbor,2] and neighbor not in active and neighbor not in new_active:
             now_active.append(neighbor)
     return node_matrix,now_active
@@ -27,11 +27,12 @@ def update_graph(edge_mat,node_mat,new_active,active):
             print("n is "+str(n))
             node_mat,now_active=neighbor_update(edge_mat,node_mat,n,active,new_active)
             print("now active is "+str(now_active))
+            print(node_mat)
             if len(now_active)!=0:
                 print(len(now_active))
                 new_active=np.append(new_active,now_active)
             new_active=new_active[1:]
             print(new_active)
             active=np.append(active,n)
-    return node_mat,new_active,active
+    return node_mat,new_active,activeS
         

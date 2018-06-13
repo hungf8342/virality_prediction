c773ad8ae5ac8b0e7a38ea8919d854b87c53d637
Frances Hung <wyfhung@gmail.com>
2018-06-13 12:05:07 -0400

    added network generator
    now possible to test linear threshold on network generator

--- models/linear_threshold.py
+++ models/linear_threshold.py
@@ -19,7 +19,8 @@ def neighbor_update(edge_matrix,node_matrix,n,active,new_active):
         if node_matrix[node_matrix[:,0]==neighbor,1]<node_matrix[node_matrix[:,0]==neighbor,2] and neighbor not in active and neighbor not in new_active:
             now_active.append(neighbor)
     return node_matrix,now_active
-    
+
+#runs linear threshold on an inputted network
 def update_graph(edge_mat,node_mat,new_active,active):
     while len(new_active)>0:
         for n in new_active:
@@ -33,6 +34,17 @@ def update_graph(edge_mat,node_mat,new_active,active):
                 new_active=np.append(new_active,now_active)
             new_active=new_active[1:]
             print(new_active)
-            active=np.append(active,n)
-    return node_mat,new_active,activeS
+            if n not in active:
+                active=np.append(active,n)
+    return node_mat,new_active,active
+
+#generates a random network with a given number of edges(size) and number of nodes(num_nodes)
+def network_gen(size,num_nodes):
+    m=np.vstack((np.random.randint(1,num_nodes,size=size),np.random.randint(1,num_nodes,size=size),np.random.rand(size,)))
+    m=np.reshape(np.ravel(m),(3,size)).T
+    for row in range(0,np.ma.size(m,axis=0)):
+        if (m[row,1]==m[row,2]):
+            m=np.delete(m,{row},axis=0)
+    node_list=np.vstack((np.asarray(range(1,num_nodes+1)),np.random.rand(num_nodes),np.zeros(num_nodes)))
+    return m,node_list.T
         

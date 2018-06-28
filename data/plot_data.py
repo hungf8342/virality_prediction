b5fd7c07a19f5625ce0c39a283d60f88dc116e1b
Andrew Baas <bosonbaas@gmail.com>
2018-06-28 11:55:18 -0400

    Throttled the updating graphlets software, and updated the plotting

--- data/plot_data.py
+++ data/plot_data.py
@@ -2,14 +2,16 @@ import sklearn.linear_model as lm
 import sklearn.model_selection as ms
 import pickle
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import sklearn.metrics as mt
 
-D = pickle.load(open("outDataLocal_rt-pol.dat", "rb"))
+D = pickle.load(open("outDataLocal_second_deg.dat", "rb"))
 
-x2Data = np.asarray(D[3])#.reshape(-1,1)
-xData = np.asarray(D[7]).reshape(-1,1)#[np.asarray(range(0, 1000)) * 33, :] #np.append(x2Data, np.asarray(D[0]), axis=1)
-yData = np.log10(np.asarray(D[3]))#[np.asarray(range(0, 1000)) * 33]
+xData = np.asarray(D[0])#.reshape(-1,1)
+#xData = np.log10(np.asarray(D[3]).reshape(-1,1) + 1)#[np.asarray(range(0, 1000)) * 33, :] #np.append(x2Data, np.asarray(D[0]), axis=1)
+yData = np.log10(np.asarray(D[4]))#[np.asarray(range(0, 1000)) * 33]
 
 inds = yData.argsort()
 
@@ -28,8 +30,8 @@ print(model.intercept_)
 print(np.asarray(model.coef_).argsort())
 plt.xlabel("exact # expected Hawkes events")
 plt.ylabel("predicted # expected Hawkes events")
-plt.title("Eigenvector Centrality to Local Expected Hawkes")
+plt.title("Local Graphlet Count to Local Expected Hawkes")
 plt.plot(yTest, model.predict(xTest), "r.", alpha=0.2, label="Linear Regression")
 plt.plot(yTest, yTest, "b-", label="Perfect Prediction")
 plt.legend()
-plt.show()
+plt.savefig("Output.png")
--- data/update_graphlets.sh
+++ data/update_graphlets.sh
@@ -2,7 +2,7 @@
 
 for g in ./graphs/*; 
 do
-  ../e_clog/omp_graphlet_count_5 -i $g -unique -all > log.txt
+  ../e_clog/omp_graphlet_count_5 -t 50 -i $g -unique -all > log.txt
   filename=$(basename $g)
   filename="${filename%.*}"
   mv $g"_all_graphlet_freqeuncy_5_omp.txt" "graphlets/"$filename".gfc"

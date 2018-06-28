e49ab6e40bdd06153c573435fdbd08b84de439fc
Andrew Baas <bosonbaas@gmail.com>
2018-06-28 11:51:47 -0400

    changed format for generating graphs

--- data/generate_graphs.py
+++ data/generate_graphs.py
@@ -1,16 +1,37 @@
+# This file takes a graph file and shuffles it
+# The format for a graph file is:
+#    number_of_nodes number_of_edges
+#    edge_1_node_1 edge_1_node_2
+#    edge_2_node_1 edge_2_node_2
+#    edge_3_node_1 edge_3_node_2
+#        ...           ...
+#
+# The nodes in the graph file need to be indexed
+# from 0 to (number_of_nodes - 1)
+
+
 import sys
 sys.path.insert(0, '../')
 import graph_manip.shuffleGraphs as sg
 import numpy as np
 import networkx as nx
 
+
 def main():
-    gDat = open("orig_graphs/socfb-Caltech36.mtx", 'rb')
+    gDat = open("orig_graphs/karate_1.txt", 'rb')
+    
+    # Read the node and edge numbers
     firstLine = gDat.readline().split()
+    
+    # Load the rest of the data into a networkx network
     graph = nx.read_edgelist(gDat)
 
-    for i in range(100):
-        nx.double_edge_swap(graph, nswap=10000, max_tries=100000)
+    for i in range(2000):
+        
+        # Shuffle the network
+        nx.double_edge_swap(graph, nswap=100, max_tries=100000)
+        
+        # Load the network into another file with proper formatting
         filename = "graphs/socfb_" + str(i) + ".dat"
         g_filename = "global_graphs/socfb_" + str(i) + ".dat"
         output = [firstLine]
